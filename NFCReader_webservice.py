571c74eabbc0f995e9ad24cd2aecf49598b51acc
Gautier Michelin <gm@ideesculture.com>
2019-06-19 10:37:19 +0200

    allow cors

--- NFCReader_webservice.py
+++ NFCReader_webservice.py
@@ -35,6 +35,7 @@ class S(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
     def do_GET(self):
